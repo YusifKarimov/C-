ae4387a3cd9df1ca5c733c6c4001fbef78dde094
YusifKarimov <40868818+YusifKarimov@users.noreply.github.com>
2018-07-22 01:01:39 +0400

    README.md
    simple example for read a file and write anoter file

---
+++ useStreamReaderWriter.cs
@@ -0,0 +1,41 @@
+# Csharp
+using System;
+using System.IO;
+
+namespace useBinarryStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader strReader;
+            StreamWriter strWriter;
+            try
+            {
+                strReader = new StreamReader("K:\\Programing\\Test files\\test.txt");
+                strWriter= new StreamWriter ("K:\\Programing\\Test files\\test10.txt");
+                do
+                {
+                    try
+                    {
+                        strWriter.WriteLine(strReader.ReadLine());
+
+                    }
+                    catch (IOException exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                        return;
+                    }
+                } while (!strReader.EndOfStream);
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return;
+            }
+            strReader.Close();
+            strWriter.Close();
+            Console.ReadKey();
+        }
+    }
+}
